78c3eb31d13b72c62cf7130d84cb92b244a4e024
vitalekp <vitalekpushkarov@gmail.com>
2023-09-21 16:16:05 +0300

    fix input border color

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -6,26 +6,21 @@ import { SITE } from '~/config.mjs';
 
 export default component$(() => {
   return (
-    <div class="flex flex-row">
-      <div class="w-[400px] h-[400px] bg-primary-gradient">
-        <h1 class="text-dark text-h2 font-bold">Text</h1>
+    <div class="flex flex-col">
+      <div class="flex flex-row">
+        <div class="w-[400px] h-[400px] bg-primary-gradient">
+          <h1 class="text-dark text-h2 font-bold">Text</h1>
+        </div>
+        <div class="w-[400px] h-[400px] bg-secondary-gradient">
+          <h1 class="text-dark text-h1">Text</h1>
+        </div>
+        <div class="w-[400px] h-[400px] bg-stroke-gradient">
+          <h1 class="text-dark text-h3">Text</h1>
+        </div>
       </div>
-      <div class="w-[400px] h-[400px] bg-secondary-gradient">
-        <h1 class="text-dark text-h1">Text</h1>
-      </div>
-      <div class="w-[400px] h-[400px] bg-stroke-gradient">
-        <h1 class="text-dark text-h3">Text</h1>
-      </div>
-
       <div class="flex gap-4 bg-slate-500 p-10 ">
-        <Input label="Input" variant="outlined" icon={true} />
-        <Input
-          label="Input"
-          // error="1"
-          // icon={true}
-          // helperText="This new error test 23.40d.303;f033;"
-          // className
-        />
+        <Input label="Input" variant="outlined" />
+        <Input label="Input" />
       </div>
     </div>
   );
