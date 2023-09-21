aa85b8d8a142f47b77158675755d910e647570ff
vitalekp <vitalekpushkarov@gmail.com>
2023-09-21 12:07:41 +0300

    create Input lego

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,26 +1,38 @@
-import { component$ } from "@builder.io/qwik";
-import type { DocumentHead } from "@builder.io/qwik-city";
+import { component$ } from '@builder.io/qwik';
+import type { DocumentHead } from '@builder.io/qwik-city';
 
-import Hero from "~/components/widgets/Hero";
-import Features from "~/components/widgets/Features";
-import Steps from "~/components/widgets/Steps";
-import FAQs from "~/components/widgets/FAQs";
-import Stats from "~/components/widgets/Stats";
-import CallToAction from "~/components/widgets/CallToAction";
+import Hero from '~/components/widgets/Hero';
+import Features from '~/components/widgets/Features';
+import Steps from '~/components/widgets/Steps';
+import FAQs from '~/components/widgets/FAQs';
+import Stats from '~/components/widgets/Stats';
+import CallToAction from '~/components/widgets/CallToAction';
 
-import { qwikSerialized } from "~/utils/qwikSerialized";
+import { qwikSerialized } from '~/utils/qwikSerialized';
 
-const IconBrandTailwind = qwikSerialized(() => import("../components/icons/IconBrandTailwind"));
-const IconApps = qwikSerialized(() => import("../components/icons/IconApps"));
-const IconRocket = qwikSerialized(() => import("../components/icons/IconRocket"));
-const IconBrandGoogle = qwikSerialized(() => import("../components/icons/IconBrandGoogle"));
-const IconBulb = qwikSerialized(() => import("../components/icons/IconBulb"));
+const IconBrandTailwind = qwikSerialized(
+  () => import('../components/icons/IconBrandTailwind')
+);
+const IconApps = qwikSerialized(() => import('../components/icons/IconApps'));
+const IconRocket = qwikSerialized(
+  () => import('../components/icons/IconRocket')
+);
+const IconBrandGoogle = qwikSerialized(
+  () => import('../components/icons/IconBrandGoogle')
+);
+const IconBulb = qwikSerialized(() => import('../components/icons/IconBulb'));
 
-import { SITE } from "~/config.mjs";
+import { SITE } from '~/config.mjs';
+import { Input } from '~/components/ui';
 
 export default component$(() => {
   return (
     <>
+      <div class="flex gap-4 bg-slate-500 p-10 ">
+        <Input label="Input" type="outlined" error={'s'} />
+        <Input label="Input" />
+      </div>
+
       <Hero />
       <Features
         highlight="Features"
@@ -28,39 +40,39 @@ export default component$(() => {
         subtitle="Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque rem aperiam, eaque ipsa quae."
         items={[
           {
-            title: "Qwik + Tailwind CSS Integration",
+            title: 'Qwik + Tailwind CSS Integration',
             description:
-              "A seamless integration between two great frameworks that offer high productivity, performance and versatility.",
+              'A seamless integration between two great frameworks that offer high productivity, performance and versatility.',
             icon: IconBrandTailwind,
           },
           {
-            title: "Ready-to-use Components",
+            title: 'Ready-to-use Components',
             description:
-              "Widgets made with Tailwind CSS ready to be used in Marketing Websites, SaaS, Blogs, Personal Profiles, Small Business...",
+              'Widgets made with Tailwind CSS ready to be used in Marketing Websites, SaaS, Blogs, Personal Profiles, Small Business...',
             icon: IconApps,
           },
           {
-            title: "Best Practices",
+            title: 'Best Practices',
             description:
-              "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi sagittis, quam nec venenatis lobortis, mi risus tempus nulla.",
+              'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi sagittis, quam nec venenatis lobortis, mi risus tempus nulla.',
           },
           {
-            title: "Excellent Page Speed",
+            title: 'Excellent Page Speed',
             description:
-              "Having a good page speed impacts organic search ranking, improves user experience (UI/UX) and increase conversion rates.",
+              'Having a good page speed impacts organic search ranking, improves user experience (UI/UX) and increase conversion rates.',
             icon: IconRocket,
           },
           {
-            title: "Search Engine Optimization (SEO)",
+            title: 'Search Engine Optimization (SEO)',
             description:
-              "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi sagittis, quam nec venenatis lobortis, mi risus tempus nulla.",
+              'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi sagittis, quam nec venenatis lobortis, mi risus tempus nulla.',
             icon: IconBrandGoogle,
           },
           {
-            title: "Open to new ideas and contributions",
+            title: 'Open to new ideas and contributions',
             description:
-              "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi sagittis, quam nec venenatis lobortis, mi risus tempus nulla.",
-            icon: IconBulb
+              'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi sagittis, quam nec venenatis lobortis, mi risus tempus nulla.',
+            icon: IconBulb,
           },
         ]}
       />
@@ -71,12 +83,12 @@ export default component$(() => {
         highlight="FAQs"
         items={[
           {
-            title: "What do I need to start?",
+            title: 'What do I need to start?',
             description:
-              "Space, the final frontier. These are the voyages of the Starship Enterprise. Its five-year mission: to explore strange new worlds. Many say exploration is part of our destiny, but it’s actually our duty to future generations.",
+              'Space, the final frontier. These are the voyages of the Starship Enterprise. Its five-year mission: to explore strange new worlds. Many say exploration is part of our destiny, but it’s actually our duty to future generations.',
           },
           {
-            title: "How to install the Qwik + Tailwind CSS template?",
+            title: 'How to install the Qwik + Tailwind CSS template?',
             description:
               "Well, the way they make shows is, they make one show. That show's called a pilot. Then they show that show to the people who make shows, and on the strength of that one show they decide if they're going to make more shows.",
           },
@@ -91,12 +103,13 @@ export default component$(() => {
               "Michael Knight a young loner on a crusade to champion the cause of the innocent. The helpless. The powerless in a world of criminals who operate above the law. Here he comes Here comes Speed Racer. He's a demon on wheels.",
           },
           {
-            title: "What is something that you would like to try again?",
+            title: 'What is something that you would like to try again?',
             description:
               "A business big enough that it could be listed on the NASDAQ goes belly up. Disappears! It ceases to exist without me. No, you clearly don't know who you're talking to, so let me clue you in.",
           },
           {
-            title: "If you could only ask one question to each person you meet, what would that question be?",
+            title:
+              'If you could only ask one question to each person you meet, what would that question be?',
             description:
               "This is not about revenge. This is about justice. A lot of things can change in twelve years, Admiral. Well, that's certainly good to know. About four years. I got tired of hearing how young I looked.",
           },
@@ -112,7 +125,7 @@ export const head: DocumentHead = {
   title: SITE.title,
   meta: [
     {
-      name: "description",
+      name: 'description',
       content: SITE.description,
     },
   ],
