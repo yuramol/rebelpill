5c3682fe373e30a44aecf0e9dbc6efc45d78e40f
vitalekp <vitalekpushkarov@gmail.com>
2023-09-26 13:01:05 +0300

    refactor

--- src/components/widgets/Header.tsx
+++ src/components/widgets/Header.tsx
@@ -20,11 +20,14 @@ export default component$(() => {
           class={`max-w-[705px] bg-dark w-full flex flex-row p-[10px] gap-[5px] rounded-full border border-gray-400`}
         >
           <ul class="flex flex-row gap-[5px] text-center text-white text-base">
+            <li class="flex  items-center">
+              <Link href="/">Home</Link>
+            </li>
             {headerNav.map(({ id, title, href }) => (
               <li
                 key={id}
                 class={[
-                  'py-[10px] px-5 bg-grey rounded-full group flex whitespace-nowrap justify-center items-center gap-5 shrink-0 overflow-hidden relative z-[1] transition-padding duration-[0.4s]',
+                  'py-[7px] px-5 bg-grey rounded-full group flex whitespace-nowrap justify-center items-center gap-5 shrink-0 overflow-hidden relative z-[1] transition-padding duration-[0.4s]',
                   'before:content-[""] before:h-full before:w-0 before:hover:w-full before:rounded-full before:absolute before:top-0 before:left-0 before:transition-all before:duration-[0.4s] before:z-[-1] before:bg-hover-gradient',
                 ]}
               >
@@ -32,7 +35,7 @@ export default component$(() => {
               </li>
             ))}
           </ul>
-          <button onClick$={scrollToTop}>Naverx</button>
+          <button onClick$={scrollToTop}>To top</button>
         </div>
       ) : null}
     </header>
