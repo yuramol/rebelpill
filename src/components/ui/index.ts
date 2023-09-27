38271e1f00e7a7b37741a7f18bca69c5ad5db225
Burban Marian <76749462+BurMarian@users.noreply.github.com>
2023-09-27 14:54:24 +0300

    feat/el-3 create button lego (#9)
    * added Button lego
    
    * pulled changes
    
    * changed variant of button from required to optional

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -1,2 +1,3 @@
 export { Typography } from './Typography';
 export { Input } from './Input';
+export { Button } from './Button';
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,7 +1,8 @@
+import { component$, $, useSignal } from '@builder.io/qwik';
 import type { DocumentHead } from '@builder.io/qwik-city';
-import { component$ } from '@builder.io/qwik';
+import { Wrapper } from '~/components/common';
+import { Button, Input } from '~/components/ui';
 import { Icon } from '~/components/icons/Icon';
-import { Input } from '~/components/ui';
 
 import { SITE } from '~/config.mjs';
 
@@ -9,8 +10,36 @@ import { SITE } from '~/config.mjs';
 import { Typography } from '~/components/ui';
 
 export default component$(() => {
+  // ! testing tabs
+  const tabs = useSignal<1 | 2>(1);
+
+  const onClick = $((e: any) => (tabs.value = e.target.tabIndex));
+
   return (
     <div class="flex flex-col">
+      <Wrapper class="flex flex-col gap-10 my-10">
+        <div class="flex flex-row gap-10 my-4">
+          <Button
+            tabIndex={1}
+            text="START COOPERATING"
+            variant={
+              tabs.value === 1 ? 'contained-secondary' : 'outlined-secondary'
+            }
+            onClick={onClick}
+          />
+          <Button
+            tabIndex={2}
+            text="learn more"
+            variant={
+              tabs.value === 2 ? 'contained-secondary' : 'outlined-secondary'
+            }
+            onClick={onClick}
+          />
+        </div>
+
+        <Button text="Send" variant="outlined-primary" />
+        <Button text="LET'S work" variant="contained-primary" />
+      </Wrapper>
       <div class="flex flex-row m-5 gap-4">
         <Typography text="H1" variant="h1" class="text-red-300" />
         <Typography text="H2" variant="h2" class="text-violet-300" />
@@ -49,13 +78,6 @@ export default component$(() => {
         <Icon icon="play" />
         <Icon icon="heart" />
         <div class="flex flex-col">
-          <div class="flex gap-4 bg-slate-500 p-10 ">
-            <Input label="Input" variant="outlined" />
-            <Input label="Input" />
-          </div>
-          <div class="w-[400px] h-[400px] bg-stroke-gradient">
-            <h1 class="text-dark text-h3">Text</h1>
-          </div>
           <div class="flex flex-row">
             <div class="w-[400px] h-[400px] bg-primary-gradient">
               <h1 class="text-dark text-h2 font-bold">Text</h1>
