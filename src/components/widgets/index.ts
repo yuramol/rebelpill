a13ab211b052a97e5942fc7a00df59aa4f58b18a
vitalekp <vitalekpushkarov@gmail.com>
2023-10-06 13:14:13 +0300

    refactor

--- src/components/widgets/index.ts
+++ src/components/widgets/index.ts
@@ -4,3 +4,4 @@ export { StepsToSuccessSection } from './StepsToSuccessSection';
 export { CleanSchemeSection } from './CleanSchemeSection';
 export { BenefitsSection } from './BenefitsSection';
 export { HeroSection } from './HeroSection';
+export { ContactSection } from './ContactSection';
