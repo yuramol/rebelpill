a68b43ec0ce9d5427cba4248e21fbcf26e3889e3
vitalekp <vitalekpushkarov@gmail.com>
2023-10-11 14:47:25 +0300

    added Typography SLOT + Adaptive for font-size

--- src/components/ui/Typography/Typography.tsx
+++ src/components/ui/Typography/Typography.tsx
@@ -1,4 +1,4 @@
-import { component$ } from '@builder.io/qwik';
+import { Slot, component$ } from '@builder.io/qwik';
 
 interface TypographyProps {
   text: string;
@@ -24,36 +24,42 @@ export const Typography = component$<TypographyProps>(
           return (
             <h1 class={`text-h1 leading-8 font-bold ${restProps.class}`}>
               {text}
+              <Slot />
             </h1>
           );
         case 'h2':
           return (
             <h2 class={`text-h2 leading-7 font-semibold ${restProps.class}`}>
               {text}
+              <Slot />
             </h2>
           );
         case 'h3':
           return (
             <h3 class={`text-h3 leading-6 font-semibold ${restProps.class}`}>
               {text}
+              <Slot />
             </h3>
           );
         case 'h4':
           return (
             <h4 class={`text-h4 leading-5 font-bold ${restProps.class}`}>
               {text}
+              <Slot />
             </h4>
           );
         case 'h5':
           return (
             <h5 class={`text-h5 leading-4 font-medium ${restProps.class}`}>
               {text}
+              <Slot />
             </h5>
           );
         case 'h6':
           return (
             <p class={`text-base leading-3 font-medium ${restProps.class}`}>
               {text}
+              <Slot />
             </p>
           );
         case 'button':
@@ -66,6 +72,7 @@ export const Typography = component$<TypographyProps>(
           return (
             <p class={`text-sm leading-2 font-normal ${restProps.class}`}>
               {text}
+              <Slot />
             </p>
           );
         case 'description':
@@ -74,6 +81,7 @@ export const Typography = component$<TypographyProps>(
               class={`text-caption leading-1 font-semibold ${restProps.class}`}
             >
               {text}
+              <Slot />
             </p>
           );
         default:
