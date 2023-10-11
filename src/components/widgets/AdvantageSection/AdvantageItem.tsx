40eb42a2b8ea9436b296594a8ebf2df8b2a694da
yuramol <yuramol86@gmail.com>
2023-10-11 12:07:42 +0300

    Add useCSSTransition

--- src/components/widgets/AdvantageSection/AdvantageItem.tsx
+++ src/components/widgets/AdvantageSection/AdvantageItem.tsx
@@ -1,4 +1,6 @@
-import { component$, useVisibleTask$ } from '@builder.io/qwik';
+import { $, component$, useSignal } from '@builder.io/qwik';
+import { useCSSTransition } from 'qwik-transition';
+
 import 'animate.css';
 import './style.css';
 import { Icon } from '~/components/ui';
@@ -13,24 +15,18 @@ interface AdvantageItemProps {
 
 export const AdvantageItem = component$<AdvantageItemProps>(
   ({ number, title, rightPosition, description, animateClass }) => {
-    useVisibleTask$(() => {
-      const accordionHeader = document.querySelectorAll('.accordion-header');
-      accordionHeader.forEach((header) => {
-        header.addEventListener('click', function () {
-          const accordionContent = header.parentElement?.querySelector(
-            '.accordion-content'
-          ) as HTMLElement;
-          const accordionMaxHeight = accordionContent.style.maxHeight;
+    const onOff = useSignal(false);
+    const newMaxHeight = useSignal(0);
+    const { stage } = useCSSTransition(onOff, { timeout: 500 });
 
-          if (accordionMaxHeight == '0px' || accordionMaxHeight.length == 0) {
-            accordionContent.style.maxHeight = `${
-              accordionContent.scrollHeight + 32
-            }px`;
-          } else {
-            accordionContent.style.maxHeight = `0px`;
-          }
-        });
-      });
+    const handleCollapse = $(() => {
+      const el = document.getElementById(`accordion-content-${number}`);
+      if (newMaxHeight.value === 0) {
+        newMaxHeight.value = el?.scrollHeight as number;
+      } else {
+        newMaxHeight.value = 0;
+      }
+      onOff.value = !onOff.value;
     });
 
     return (
@@ -39,9 +35,12 @@ export const AdvantageItem = component$<AdvantageItemProps>(
           rightPosition ? 'md:ml-auto ' : 'md:mr-auto '
         } ${animateClass}`}
       >
-        <div class="transition ">
+        <div>
           <p class="text-sm mb-5">{`00${number}`}</p>
-          <div class="accordion-header cursor-pointer transition flex space-x-5 items-center h-16">
+          <div
+            class="accordion-header cursor-pointer transition flex space-x-5 items-center h-16"
+            onClick$={handleCollapse}
+          >
             <h1 class="text-h3 md:text-h1 uppercase font-semibold">{title}</h1>
             <div class="h-7 w-7 md:h-11 md:w-11">
               <Icon
@@ -51,9 +50,17 @@ export const AdvantageItem = component$<AdvantageItemProps>(
                 color="#ff5c00"
               />
             </div>
-            <div class="accordion-content pt-0 overflow-hidden max-h-0">
-              <p class="pr-7 md:pr-0">{description}</p>
-            </div>
+          </div>
+          <div
+            id={`accordion-content-${number}`}
+            class="accordion-content pt-0 overflow-hidden max-h-0"
+            style={{
+              transition: 'max-height 0.5s ease-out',
+              maxHeight:
+                stage.value === 'enterTo' ? `${newMaxHeight.value}px` : 0,
+            }}
+          >
+            <p class="pr-7 md:pr-0">{description}</p>
           </div>
         </div>
       </div>
