eca4cc7fa356148cf0b375126b1de226f51ba370
yuramol <yuramol86@gmail.com>
2023-10-12 13:06:46 +0300

    Fix Typography texts

--- src/components/widgets/CalculateSection/CalculateSection.tsx
+++ src/components/widgets/CalculateSection/CalculateSection.tsx
@@ -8,7 +8,9 @@ import { Wrapper } from '~/components/common';
 export const CalculateSection = component$(() => {
   return (
     <Wrapper class="flex flex-col items-center justify-center w-full bg-dark">
-      <Typography variant="button" text="Calculate your profit" class="mb-20" />
+      <Typography variant="button" class="mb-20">
+        Calculate your profit
+      </Typography>
       <div class="flex flex-col md:flex-row w-full items-center justify-center">
         <RevenueModel />
         <Profit />
