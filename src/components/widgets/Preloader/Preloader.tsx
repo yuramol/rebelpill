b62be647c4f3616c193a450fe379cd0108db0b81
vitalekp <80340001+vitalekp@users.noreply.github.com>
2023-10-13 12:34:42 +0300

    Fix the preloader (#28)

--- src/components/widgets/Preloader/Preloader.tsx
+++ src/components/widgets/Preloader/Preloader.tsx
@@ -1,4 +1,9 @@
-import { component$, useStyles$, useVisibleTask$ } from '@builder.io/qwik';
+import {
+  component$,
+  useSignal,
+  useStyles$,
+  useVisibleTask$,
+} from '@builder.io/qwik';
 
 import Pill from '~/assets/images/Pill.png?jsx';
 
@@ -7,38 +12,42 @@ import { BottomTop, FinalLogo, StartTop } from './svg';
 import styles from './style.css?inline';
 
 export const Preloader = component$(() => {
+  const isPageLoaded = useSignal(false);
+
   useVisibleTask$(() => {
     const preloader = document.getElementById('preloader');
     if (preloader) {
       preloader.classList.add('active');
     }
     setTimeout(() => {
-      if (preloader) {
-        preloader.style.display = 'none';
-      }
+      isPageLoaded.value = true;
     }, 3000);
   });
 
   useStyles$(styles);
 
   return (
-    <div class="preloader px-5" id="preloader">
-      <div class="preloader_start">
-        <div class="preloader_start-top">
-          <StartTop />
+    <>
+      {!isPageLoaded.value ? (
+        <div class="preloader px-5" id="preloader">
+          <div class="preloader_start">
+            <div class="preloader_start-top">
+              <StartTop />
+            </div>
+            <div class="preloader_start-bottom">
+              <BottomTop />
+            </div>
+          </div>
+          <div class="preloader_final">
+            <div class="preloader_final-logo">
+              <FinalLogo />
+            </div>
+            <div class="preloader_final-pill">
+              <Pill />
+            </div>
+          </div>
         </div>
-        <div class="preloader_start-bottom">
-          <BottomTop />
-        </div>
-      </div>
-      <div class="preloader_final">
-        <div class="preloader_final-logo">
-          <FinalLogo />
-        </div>
-        <div class="preloader_final-pill">
-          <Pill />
-        </div>
-      </div>
-    </div>
+      ) : null}
+    </>
   );
 });
