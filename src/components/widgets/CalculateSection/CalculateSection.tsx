1958e3206f38f48c49a4bb4215db0c03e27ab0a0
vitalekp <80340001+vitalekp@users.noreply.github.com>
2023-10-13 12:20:56 +0300

    Feat/RP- 21 create a divider for the section (#27)
    * create Divider
    
    * fix carousel start slide

--- src/components/widgets/CalculateSection/CalculateSection.tsx
+++ src/components/widgets/CalculateSection/CalculateSection.tsx
@@ -7,14 +7,16 @@ import { Wrapper } from '~/components/common';
 
 export const CalculateSection = component$(() => {
   return (
-    <Wrapper class="flex flex-col items-center justify-center w-full bg-dark">
-      <Typography variant="button" class="mb-20">
-        Calculate your profit
-      </Typography>
-      <div class="flex flex-col md:flex-row w-full items-center justify-center">
-        <RevenueModel />
-        <Profit />
-      </div>
-    </Wrapper>
+    <section>
+      <Wrapper class="flex flex-col items-center justify-center w-full bg-dark py-[180px]">
+        <Typography variant="button" class="mb-20">
+          Calculate your profit
+        </Typography>
+        <div class="flex flex-col md:flex-row w-full items-center justify-center">
+          <RevenueModel />
+          <Profit />
+        </div>
+      </Wrapper>
+    </section>
   );
 });
