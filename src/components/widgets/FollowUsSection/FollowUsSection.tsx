a3f976f2fd53252af669e39a9d825338b90eca8f
vitalekp <vitalekpushkarov@gmail.com>
2023-10-17 18:49:55 +0300

    added TikTok embed

--- src/components/widgets/FollowUsSection/FollowUsSection.tsx
+++ src/components/widgets/FollowUsSection/FollowUsSection.tsx
@@ -1,18 +1,18 @@
-import Swiper from 'swiper';
-import { Autoplay } from 'swiper/modules';
-import { Link } from '@builder.io/qwik-city';
-import { component$, useStyles$, useVisibleTask$ } from '@builder.io/qwik';
+import Swiper from "swiper";
+import { Autoplay } from "swiper/modules";
+import { Link } from "@builder.io/qwik-city";
+import { component$, useStyles$, useVisibleTask$ } from "@builder.io/qwik";
 
-import styles from './style.css?inline';
+import styles from "./style.css?inline";
 
-import { Icon, Typography } from '~/components/ui';
+import { Icon, Typography } from "~/components/ui";
 
-const TIKTOKLINK = 'https://www.tiktok.com/@it.dropshipper?_t=8gRRRNlxGoo&_r=1';
+const TIKTOKLINK = "https://www.tiktok.com/@it.dropshipper?_t=8gRRRNlxGoo&_r=1";
 
 export const FollowUsSection = component$(() => {
   useVisibleTask$(() => {
-    new Swiper('.swiper-follow', {
-      slidesPerView: 'auto',
+    new Swiper(".swiper-follow", {
+      slidesPerView: "auto",
       loop: true,
       initialSlide: 1,
       autoplay: {
@@ -28,7 +28,7 @@ export const FollowUsSection = component$(() => {
   return (
     <section
       class={[
-        'w-full  flex overflow-hidden relative py-[120px] md:py-[170px]',
+        "w-full  flex overflow-hidden relative py-[120px] md:py-[170px]",
         "before:content-[''] before:bg-primary-gradient-mirror before:absolute before:inset-0 before:-scale-x-100",
       ]}
     >
@@ -40,17 +40,74 @@ export const FollowUsSection = component$(() => {
                 <div
                   class="w-full h-full overflow-hidden bg-dark"
                   style={{
-                    transform: 'rotate(5deg)',
+                    transform: "rotate(5deg)",
                   }}
                 >
-                  <iframe
-                    src="https://www.youtube.com/embed/het2TX4eurA?si=krumrz_Q5-U3Ztjd"
-                    title="YouTube video player"
-                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                    allowFullScreen
-                    key={index}
-                    class="h-full w-full"
-                  ></iframe>
+                  <blockquote
+                    class="tiktok-embed"
+                    cite="https://www.tiktok.com/@it.dropshipper/video/7276546441470922026"
+                    data-video-id="7276546441470922026"
+                    style="max-width: 605px;min-width: 325px;"
+                  >
+                    {" "}
+                    <section>
+                      {" "}
+                      <a
+                        target="_blank"
+                        title="@it.dropshipper"
+                        href="https://www.tiktok.com/@it.dropshipper?refer=embed"
+                      >
+                        @it.dropshipper
+                      </a>{" "}
+                      Fill the form and start your passive income in a few days!
+                      Unlock your full earning potential and achieve financial
+                      freedom. Stop you 9-5&#47;5-2 slavery. Join to the IT
+                      Dropshipping rebelion!{" "}
+                      <a
+                        title="itdropshipping"
+                        target="_blank"
+                        href="https://www.tiktok.com/tag/itdropshipping?refer=embed"
+                      >
+                        #itdropshipping
+                      </a>{" "}
+                      <a
+                        title="rebelpill"
+                        target="_blank"
+                        href="https://www.tiktok.com/tag/rebelpill?refer=embed"
+                      >
+                        #rebelpill
+                      </a>{" "}
+                      <a
+                        title="sidehustle"
+                        target="_blank"
+                        href="https://www.tiktok.com/tag/sidehustle?refer=embed"
+                      >
+                        #sidehustle
+                      </a>{" "}
+                      <a
+                        title="tiktech"
+                        target="_blank"
+                        href="https://www.tiktok.com/tag/tiktech?refer=embed"
+                      >
+                        #tiktech
+                      </a>{" "}
+                      <a
+                        title="itprofessional"
+                        target="_blank"
+                        href="https://www.tiktok.com/tag/itprofessional?refer=embed"
+                      >
+                        #itprofessional
+                      </a>{" "}
+                      <a
+                        target="_blank"
+                        title="♬ original sound - It Dropshipper"
+                        href="https://www.tiktok.com/music/original-sound-7276546465445530410?refer=embed"
+                      >
+                        ♬ original sound - It Dropshipper
+                      </a>{" "}
+                    </section>{" "}
+                  </blockquote>{" "}
+                  <script async src="https://www.tiktok.com/embed.js"></script>
                 </div>
               </div>
             ))}
