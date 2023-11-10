84ff27770aeb53ce7bb65506ae42f93260f2185c
vitalekp <80340001+vitalekp@users.noreply.github.com>
2023-11-10 12:39:15 +0200

    fix mobile menu scroll to section (#36)

--- src/components/widgets/helpers.ts
+++ src/components/widgets/helpers.ts
@@ -1,5 +1,12 @@
+import { $ } from '@builder.io/qwik';
+
 export const headerNav = [
   { id: 'cleanSchemeSection', title: 'How it works' },
   { id: 'contactSection', title: 'Start making money!' },
   { id: 'calculateSection', title: 'Calculate profit' },
 ];
+
+export const scrollToElement = $((elementId: string) => {
+  const element = document.getElementById(elementId);
+  element?.scrollIntoView();
+});
