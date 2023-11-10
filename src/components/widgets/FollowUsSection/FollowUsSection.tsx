16252f19b08d04e5b38d51709331db401da46082
vitalekp <80340001+vitalekp@users.noreply.github.com>
2023-11-10 11:51:11 +0200

    show FollowUs section after scroll (#35)

--- src/components/widgets/FollowUsSection/FollowUsSection.tsx
+++ src/components/widgets/FollowUsSection/FollowUsSection.tsx
@@ -1,7 +1,14 @@
 import Swiper from 'swiper';
 import { Autoplay } from 'swiper/modules';
 import { Link } from '@builder.io/qwik-city';
-import { component$, useStyles$, useVisibleTask$ } from '@builder.io/qwik';
+import {
+  $,
+  component$,
+  useOnDocument,
+  useSignal,
+  useStyles$,
+  useVisibleTask$,
+} from '@builder.io/qwik';
 
 import styles from './style.css?inline';
 
@@ -11,6 +18,16 @@ import { tikTokVideos } from './helpers';
 const TIKTOKLINK = 'https://www.tiktok.com/@it.dropshipper?_t=8gRRRNlxGoo&_r=1';
 
 export const FollowUsSection = component$(() => {
+  const scrollPosition = useSignal<any>(0);
+
+  useOnDocument(
+    'scroll',
+    $(() => {
+      const scrollY = document.documentElement.scrollTop;
+      scrollPosition.value = scrollY;
+    })
+  );
+
   useVisibleTask$(() => {
     new Swiper('.swiper-follow', {
       slidesPerView: 'auto',
@@ -33,100 +50,51 @@ export const FollowUsSection = component$(() => {
         "before:content-[''] before:bg-primary-gradient-mirror before:absolute before:inset-0 before:-scale-x-100",
       ]}
     >
-      <div class="flex w-full items-center justify-center">
-        <div class="swiper-follow w-full h-full max-w-[1440px] rotate-[353deg]">
-          <div class="swiper-wrapper flex items-center">
-            {tikTokVideos.map(
-              ({ cite, dataVideoId, href, id, description }) => (
-                <div key={id} class="swiper-slide adaptive-size mx-6">
-                  <div
-                    class="w-full h-full overflow-hidden bg-dark"
-                    style={{
-                      transform: 'rotate(5deg)',
-                    }}
-                  >
-                    <blockquote
-                      class="tiktok-embed w-full h-full"
-                      cite={cite}
-                      data-video-id={dataVideoId}
+      {scrollPosition.value > 2000 ? (
+        <>
+          <div class="flex w-full items-center justify-center">
+            <div class="swiper-follow w-full h-full max-w-[1440px] rotate-[353deg]">
+              <div class="swiper-wrapper flex items-center">
+                {tikTokVideos.map(({ cite, dataVideoId, id }) => (
+                  <div key={id} class="swiper-slide adaptive-size mx-6">
+                    <div
+                      class="w-full h-full overflow-hidden bg-dark"
+                      style={{
+                        transform: 'rotate(5deg)',
+                      }}
                     >
-                      <section>
-                        <a
-                          target="_blank"
-                          title="@it.dropshipper"
-                          href="https://www.tiktok.com/@it.dropshipper?refer=embed"
-                        >
-                          @it.dropshipper
-                        </a>
-                        {description}
-                        <a
-                          title="itdropshipping"
-                          target="_blank"
-                          href="https://www.tiktok.com/tag/itdropshipping?refer=embed"
-                        >
-                          #itdropshipping
-                        </a>
-                        <a
-                          title="rebelpill"
-                          target="_blank"
-                          href="https://www.tiktok.com/tag/rebelpill?refer=embed"
-                        >
-                          #rebelpill
-                        </a>
-                        <a
-                          title="sidehustle"
-                          target="_blank"
-                          href="https://www.tiktok.com/tag/sidehustle?refer=embed"
-                        >
-                          #sidehustle
-                        </a>
-                        <a
-                          title="tiktech"
-                          target="_blank"
-                          href="https://www.tiktok.com/tag/tiktech?refer=embed"
-                        >
-                          #tiktech
-                        </a>
-                        <a
-                          title="itprofessional"
-                          target="_blank"
-                          href="https://www.tiktok.com/tag/itprofessional?refer=embed"
-                        >
-                          #itprofessional
-                        </a>
-                        <a
-                          target="_blank"
-                          title="♬ original sound - It Dropshipper"
-                          href={href}
-                        >
-                          ♬ original sound - It Dropshipper
-                        </a>
-                      </section>
-                    </blockquote>
-                    <script
-                      async
-                      src="https://www.tiktok.com/embed.js"
-                    ></script>
+                      <blockquote
+                        class="tiktok-embed w-full h-full"
+                        cite={cite}
+                        data-video-id={dataVideoId}
+                      >
+                        <section></section>
+                      </blockquote>
+                      <script
+                        async
+                        src="https://www.tiktok.com/embed.js"
+                      ></script>
+                    </div>
                   </div>
-                </div>
-              )
-            )}
+                ))}
+              </div>
+            </div>
           </div>
-        </div>
-      </div>
-      <div class="absolute z-10 left-[50%] md:left-[70%] bottom-[10%] md:bottom-[18%] rotate-[-4deg] translate-x-[-50%] translate-y-0">
-        <Link
-          href={TIKTOKLINK}
-          target="_blank"
-          rel="noopener noreferrer"
-          class="flex flex-row items-center gap-2"
-        >
-          <Icon icon="tikTok" />
-          <Typography variant="body1" class="whitespace-nowrap uppercase">
-            Follow us on Tik tok
-          </Typography>
-        </Link>
-      </div>
+          <div class="absolute z-10 left-[50%] md:left-[70%] bottom-[10%] md:bottom-[18%] rotate-[-4deg] translate-x-[-50%] translate-y-0">
+            <Link
+              href={TIKTOKLINK}
+              target="_blank"
+              rel="noopener noreferrer"
+              class="flex flex-row items-center gap-2"
+            >
+              <Icon icon="tikTok" />
+              <Typography variant="body1" class="whitespace-nowrap uppercase">
+                Follow us on Tik tok
+              </Typography>
+            </Link>
+          </div>
+        </>
+      ) : null}
     </section>
   );
 });
